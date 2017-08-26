181ab830c20bdab3a3bccc673005236f85716386
Yang Qinfeng <826394735@qq.com>
2017-08-26 18:23:19 +0800

    修改内容： 1、上传的相关代码

---
+++ src/main/java/cotroller/upload/PictureController.java
@@ -0,0 +1,28 @@
+package cotroller.upload;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import service.PictureService;
+import utils.JsonUtils;
+
+import java.util.Map;
+
+/**
+ * Created by Yang Qinfeng on 2017/8/26.
+ */
+public class PictureController {
+
+    @Autowired
+    private PictureService pictureService;
+
+    @RequestMapping("/pic/upload")
+    @ResponseBody
+    public String pictureUpload(MultipartFile uploadFile) {
+        Map result = pictureService.uploadPicture(uploadFile);
+        //为了保证功能的兼容性，需要把Result转换成json格式的字符串。
+        String json = JsonUtils.objectToJson(result);
+        return json;
+    }
+}
