36bda081a916836dd8c1f6e7b43023c7dc8cf295
Yang Qinfeng <826394735@qq.com>
2017-08-26 18:21:34 +0800

    修改内容： bug修改

--- src/main/java/utils/JsonUtils.java
+++ src/main/java/utils/JsonUtils.java
@@ -1,6 +1,7 @@
 package utils;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.util.List;
